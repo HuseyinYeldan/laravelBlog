43e52995fd669e147489c23e31f4a8d5ea01abd0
Hüseyin Yeldan <yeldanhuseyin1@gmail.com>
2023-12-13 10:02:30 +0300

    Service provider method for API

--- app/Services/Newsletter.php
+++ app/Services/Newsletter.php
@@ -2,27 +2,20 @@
 
 namespace App\Services;
 
+use MailchimpMarketing\ApiClient;
+
 class Newsletter{
+
+    public function __construct(protected ApiClient $client){
+
+    }
 
     public function subscribe(string $email, string $list=null){
         $list ??=config('services.mailchimp.lists.subscribers');
 
-        return $this->client()->lists->addListMember($list,[
+        return $this->client->lists->addListMember($list,[
             'email_address' => $email,
             'status' => 'subscribed'
-        ]);
-    }
-
-    public function client(){
-        $mailchimp = new \MailchimpMarketing\ApiClient();
-
-        request()->validate([
-            'email' => 'required|email|min:6|max:128'
-        ]);
-    
-        return $mailchimp->setConfig([
-            'apiKey' => config('services.mailchimp.key'),
-            'server' => 'us21'
         ]);
     }
 
